6031fada957d932c683dc52bd3daed1885888748
Fabrício G. M. de Carvalho <phabricio@hotmail.com>
2023-07-26 17:18:02 -0300

    Repository creation. Examples of definitions

---
+++ classes.ts
@@ -0,0 +1,20 @@
+/* Examples of class definition and inheritance */
+
+
+class Computer{
+    screen_width: number;
+    scree_height: number;
+}
+
+class Laptop extends Computer{
+    batery_type: string;
+}
+
+let my_lap = new Laptop();
+my_lap.batery_type="Li-ion";
+my_lap.scree_height = 20;
+my_lap.screen_width = 30;
+
+console.log(my_lap.batery_type);
+console.log(my_lap.screen_width);
+console.log(my_lap.scree_height);
---
+++ enumerations.ts
@@ -0,0 +1,10 @@
+/* Exemplifying enumerations */
+
+enum ControlDirections{
+    left = 1,
+    right = -1
+  }
+
+ console.log(ControlDirections.left);
+ console.log(ControlDirections.right);
+ 
